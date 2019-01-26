8c48b320e696b8cd0d8e81b24e73a2f9cb58e09b
Isaac Quek <isaacquek97@gmail.com>
2019-01-26 19:47:07 +0800

    Fixed Admin not showing due to placing after resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :microposts          #only: [:create, :destroy]
 
-  #get    '/readme',    to: 'static_pages#readme'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
+  get   '/users/admin',     to: 'users#admin'
   get    '/signup',  to: 'users#new'
   post    '/signup',  to: 'users#create'
+
   get    '/login',    to: 'sessions#new'
   post   '/login',    to: 'sessions#create'
   delete '/logout',   to: 'sessions#destroy'
-  get   '/users/admin',     to: 'users#admin'
 
   get 'tags/:tag', to: 'microposts#index', as: :tag
   #the error reflects whatever the route is typed, but the args after /:tag 
   #needs some argument to work
+  resources :users
+  resources :microposts          #only: [:create, :destroy]
+
+
+
+    #get    '/readme',    to: 'static_pages#readme'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
   root   'static_pages#home'
 end
